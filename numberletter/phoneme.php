8bb014e3c8cdd6f9ca7b0c15d083196dc2910c2f
rmcelheny <rosaNEW@Rosas-MacBook-Pro.local>
2018-02-26 19:11:27 -0500

    got it workin

--- numberletter/phoneme.php
+++ numberletter/phoneme.php
@@ -2,18 +2,20 @@
 <html>
 <head>
 	<title>test</title>
-<!-- 	<style>
-
-	.fade {
-		animation-name:fade;
-	}
-
-	@keyframes fade {
-		from {opacity:1;}
-		to {opacity:2;}
-	}
-
-	</style> -->
+	<style>
+/*		body {
+			background-color:blue;
+		}*/
+		img {
+			animation-name:fade;
+			animation-iteration-count: infinite;
+			animation-direction:alternate;
+		}
+		@keyframes fade {
+			from {opacity:1;}
+			to {opacity:0;}
+		}
+	</style>
 </head>
 <body>
  
@@ -21,12 +23,14 @@
 
 	$letter = $_GET['letter'];
 	$number = $_GET['number'];
-
 	$width = $number*10;
 	$widthpx = $width."px";
-	echo "<img style='width:$widthpx;' src='assets/$letter.jpg'>";
+	$duration = (100/$number)."s";
+	echo "<img  src='assets/$letter.jpg'>";
+	echo "<head><style> img {width:$widthpx; animation-duration:$duration;}</style></head>"
 
 ?>
+
 
 </body>
 </html>
